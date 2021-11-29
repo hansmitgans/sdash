61934966fc9e3f9d2a13b79f9a7c41262f88dddb
hansmitgans <40957539+hansmitgans@users.noreply.github.com>
2021-11-29 09:57:00 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,3 @@
- Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
 import plotly.express as px
 import pandas as pd
